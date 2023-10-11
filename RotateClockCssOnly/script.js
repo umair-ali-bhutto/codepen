7aa8427a24888d39f67493e76acac6c21d75df05
Umair Ali <ibaumairalibhutto@gmail.com>
2023-10-11 12:24:10 +0500

    added Clock

---
+++ RotateClockCssOnly/script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code to set SCSS variables here
+    const now = new Date();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
+const root = document.documentElement;
+
+// root.style.setProperty('--hours', hours);
+// root.style.setProperty('--minutes', minutes);
+// root.style.setProperty('--seconds', seconds);
+
+// // Set the values of your SCSS variables
+// root.style.setProperty('--hour', '5');
+// root.style.setProperty('--minute', '31');
+// root.style.setProperty('--second', '45');
+  });
+
+
+
