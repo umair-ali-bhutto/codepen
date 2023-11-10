33ceba8fadd0d0c13918bd05a17809ea13f33850
Umair Ali <ibaumairalibhutto@gmail.com>
2023-11-10 11:11:39 +0500

    started conversion of css clock to js clock

--- pens/RotateClockCssOnly/script.js
+++ pens/RotateClockCssOnly/script.js
@@ -1,22 +1,43 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Your code to set SCSS variables here
-    const now = new Date();
-const hours = now.getHours();
-const minutes = now.getMinutes();
-const seconds = now.getSeconds();
+// ALL Varibles
+var hour = 12;
+var minute = 31;
+var second = 45;
 
-const root = document.documentElement;
+var radius = '75px';
+var animationTiming = 'cubic-bezier(1,0,1,0)';
 
-// root.style.setProperty('--hours', hours);
-// root.style.setProperty('--minutes', minutes);
-// root.style.setProperty('--seconds', seconds);
+var colorActive = '#FFF';
+var colorInactive = 'rgba(#FFF, 0.2)';
 
-// // Set the values of your SCSS variables
-// root.style.setProperty('--hour', '5');
-// root.style.setProperty('--minute', '31');
-// root.style.setProperty('--second', '45');
-  });
+var fontSize = '35px';
+
+var hourTens = Math.floor(hour / 10);
+var hourOnes = hour % 10;
+var minuteTens = Math.floor(minute / 10);
+var minuteOnes = minute % 10;
+var secondTens = Math.floor(second / 10);
+var secondOnes = second % 10;
+
+var pi = 3.14159265359;
+var precision = 10;
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Your code to set SCSS variables here
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+});
+
+
+function pow(base, exp) {
+
+}
 
 
 
