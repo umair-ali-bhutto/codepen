c05d050360a35d28e4b4e8aa267b14fbf5955442
Umair Ali <ibaumairalibhutto@gmail.com>
2023-11-15 13:00:32 +0500

    converted css clock to js clock

--- pens/RotateClockCssOnly/script.js
+++ pens/RotateClockCssOnly/script.js
@@ -1,8 +1,14 @@
+// GETTING TODAYS TIME
+const now = new Date();
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds();
+
 
 // ALL Varibles
-var hour = 12;
-var minute = 31;
-var second = 45;
+var hour = hours;
+var minute = minutes;
+var second = 0;
 
 // var radius = '75px';
 var radius = 75;
@@ -137,36 +143,151 @@ function generateAnimation(name, numberOfDigits) {
 
 
 
+function assignAnimations() {
+  let CSS = '';
+  let temp;
+
+  for (let i = 1; i <= 3; i++) {
+    let index = i + hourTens;
+    if (index > 3) { index = index - 3; }
+    temp = -((3600 * hourOnes) + (600 * minuteTens) + (60 * minuteOnes) + (10 * secondTens) + secondOnes);
+    CSS += `.hourTens li:nth-child(${index}){
+      animation:three${i} 108000s ${animationTiming} infinite;
+      animation-delay:${temp}s;
+    }`;
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    let index = i + hourOnes;
+    if (index > 10) { index = index - 10; }
+    temp = -((600 * minuteTens) + (60 * minuteOnes) + (10 * secondTens) + secondOnes);
+    CSS += `.hourOnes li:nth-child(${index}){
+      animation:ten${i} 36000s ${animationTiming} infinite;
+      animation-delay:${temp}s;
+    }`;
+  }
+
+  for (let i = 1; i <= 6; i++) {
+    let index = i + minuteTens;
+    if (index > 6) { index = index - 6; }
+    temp = -((60 * minuteOnes) + (10 * secondTens) + secondOnes);
+    CSS += `.minuteTens li:nth-child(${index}){
+      animation:six${i} 3600s ${animationTiming} infinite;
+      animation-delay:${temp}s;
+    }`;
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    let index = i + minuteTens;
+    if (index > 10) { index = index - 10; }
+    temp = -((10 * secondTens) + secondOnes);
+    CSS += `.minuteOnes li:nth-child(${index}){
+      animation:ten${i} 600s ${animationTiming} infinite;
+      animation-delay:${temp}s;
+    }`;
+  }
+
+  for (let i = 1; i <= 6; i++) {
+    let index = i + minuteTens;
+    if (index > 6) { index = index - 6; }
+    temp = -(secondOnes);
+    CSS += `.secondTens li:nth-child(${index}){
+      animation:six${i} 60s ${animationTiming} infinite;
+      animation-delay:${temp}s;
+    }`;
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    let index = i + minuteTens;
+    if (index > 10) { index = index - 10; }
+    CSS += `.secondOnes li:nth-child(${index}){
+      animation:ten${i} 10s cubic-bezier(0.9, 0, 0.9, 0) infinite;
+    }`;
+  }
+
+  styleElement.innerHTML += CSS;
+}
 
 
+function staticCss() {
+  let CSS = '';
 
+  CSS = `
+  
+html {
+  height: 100%;
+}
 
+body {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  background: #DB6713;
+  background: linear-gradient(45deg, #dba013, #df2445);
+  font-family: "Lato", sans-serif;
+}
 
+div {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 
+ul {
+  position: relative;
+  width: 30px;
+  height: 35px;
+  margin: 0;
+  padding: 0;
+  float: left;
+  list-style-type: none;
+  font-size: 35px;
+  font-weight: bold;
+}
+ul:nth-of-type(even) {
+  margin-right: 15px;
+  padding-right: 15px;
+}
+ul:nth-of-type(even)::after {
+  content: ":";
+  position: absolute;
+  right: 0;
+  color: #FFF;
+  line-height: 1;
+}
+ul:last-of-type::after {
+  display: none;
+}
+ul li {
+  position: absolute;
+  backface-visibility: hidden;
+  color: rgba(255, 255, 255, 0.2);
+  font-size: 35px;
+  line-height: 1;
+}
+  `;
 
+  styleElement.innerHTML += CSS;
 
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
 
   generateAnimation("ten", 10);
   generateAnimation("six", 6);
   generateAnimation("three", 3);
 
 
+  assignAnimations();
 
+  staticCss();
+
+  console.log(styleElement.innerHTML);
   document.head.appendChild(styleElement);
-
-
-
 });
-
-
-
-
 
